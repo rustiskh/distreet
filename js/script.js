3aea1b550bc5df784f3a558aa7dbf47077b19434
Rustam <rustam1222@gmail.com>
2022-07-14 17:56:44 +0200

    header fixes (+bg) and bx-szng:brdr-bx for body

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.mobile-menu'),
     menuItem = document.querySelectorAll('.mobile-menu__item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    bgMobile = document.querySelector('.mobile-menu__background');
 
     hamburger.addEventListener('click', () => {
         hamburger.classList.toggle('hamburger_active');
+        bgMobile.classList.toggle('mobile-menu__background_active');
         menu.classList.toggle('mobile-menu_active');
     });
 
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.toggle('hamburger_active');
+            bgMobile.classList.toggle('mobile-menu__background_active');
             menu.classList.toggle('mobile-menu_active');
         });
     });
